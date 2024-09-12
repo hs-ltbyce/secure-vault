8e333bbd044788852cce58dc6d7c545f9cd3d10f
hs-ltbyce <yechangqing@info-water.com>
2024-09-12 19:12:24 +0800

    chore(i18n): support chinese and update default config

--- frontend/src/translations/index.ts
+++ frontend/src/translations/index.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import * as en from './en';
-import * as fr from './fr';
+import * as cn from './zh-cn';
 
 type TupleUnion<U extends string, R extends unknown[] = []> = {
   [S in U]: Exclude<U, S> extends never
@@ -10,7 +10,7 @@ type TupleUnion<U extends string, R extends unknown[] = []> = {
     : TupleUnion<Exclude<U, S>, [...R, S]>;
 }[U];
 
-const ns = Object.keys(en) as TupleUnion<keyof typeof en>;
+const ns = Object.keys(cn) as TupleUnion<keyof typeof cn>;
 
 export const defaultNS = ns[0];
 
@@ -19,10 +19,10 @@ void i18n.use(initReactI18next).init({
   defaultNS,
   resources: {
     en,
-    fr,
+    cn,
   },
-  lng: 'en',
-  fallbackLng: 'en',
+  lng: 'cn',
+  fallbackLng: 'cn',
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
   },
