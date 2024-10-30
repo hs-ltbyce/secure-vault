ef14214e0dd050331e65e76bdd2cfb86c9f1e902
hs-ltbyce <yechangqing@info-water.com>
2024-10-30 16:59:35 +0800

    chore(rename): Rename the project name from frontend to secure-vault

--- frontend/ios/securevaultTests/securevaultTests.m
+++ frontend/ios/securevaultTests/securevaultTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface frontendTests : XCTestCase
+@interface securevaultTests : XCTestCase
 
 @end
 
-@implementation frontendTests
+@implementation securevaultTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
